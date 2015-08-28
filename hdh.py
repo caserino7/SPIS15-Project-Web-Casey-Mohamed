648b9e091d1a24b00ac5d6d647c0d2c7503dd1e8
Mo Al Elew <malelew@ucsd.edu>
2015-08-27 22:18:02 -0700

    worked on html

--- hdh.py
+++ hdh.py
@@ -8,6 +8,20 @@ app = Flask(__name__)
 @app.route('/')
 def renderMain():
     return render_template('main.html')
+#add ~ 9 more pages for 3 meals at every hall
+
+@app.route('/canyonvista')
+def renderCV():
+    return render_template('CV.html')
+
+@app.route('/64degrees')
+def render64d():
+    return render_template('64degrees.html')
+
+@app.route('/pines')
+def renderPines():
+    return render_template('pines.html')
+
 
 if __name__=="__main__":
     app.run(debug=False, host="0.0.0.0", port=55555)
--- ratings.py
+++ ratings.py
@@ -1,5 +1,16 @@
-ratings = {"Canyon Vista": {
-"Aztec Black Beans":True, "Vegetarian Chili": True,"Vegetarian Pizza": False
+ratings = {"Canyon Vista": {'Assorted Summer Scones', 'Beans', 'Blueberry Muffin', 'Cage Free Scrambled Eggs', 'Carrots & Jalapenos', 'Flatbread Breakfast Pizza',
+                'Steelcut Oatmeal', 'Tater Barrels', 'Veggie Sausage',
+                'Whole Wheat English Muffin', 'Zucchini Breakfast Muffins',
+                'Fresh Tortillas', 'Gluten Friendly Bread', 'Pancakes', 'Pork Sausage Links',
+                'Scrambled Egg Whites', 'Smoked Bacon', 'Cereal with Milk', 'Cut Fresh Fruit', 'Whole Apple', 'Whole Banana',
+                'Whole Orange', 'Yogurt Bar', 'Chciken Picatta', 'Cheese Enchilada', 'Aztec Black Beans', 'Cilantro Rice', 'Steamed Broccoli w/ Almonds',
+                'Summer Roast Beef Wrap', 'Tuna Super Sub', 'Vegetarian Super Sub', 'Cheeseburger', 'Gardein Burger', 'Grilled Cheese Sandwich', 'Hamburger',
+                'Hot Dog', 'Crispy French Fries', 'Cheese Pizza', 'Pepperoni Pizza', 'Vegetarian Pizza', 'BYO Pasta Bar (Alfredo Nutritional Sample)', 'Fresh Salad Bar',
+                'Jalapeno Cornbread', 'Clam Chowder', 'Vegetarian Chili', 'Butterscotch Pudding', 'Chocolate Cake', 'Chocolate Pudding', 'Rice Krispy Treats',
+                'Herb Crusted Red Snapper', 'Homestyle Pot Roast', 'Baked Potato Bar', 'Steamed Brown Rice', 'Sweet Chili Carrots', 'Cheese Pizza', 'Pepperoni Pizza',
+                'Vegetarian Pizza', 'BYO Pasta Bar (Alfredo Nutritional Sample)', 'Fresh Salad Bar', 'Jalapeno Cornbread', 'Clam Chowder', 'Vegetarian Chili',
+                'Butterscotch Pudding', 'Chocolate Cake', 'Chocolate Pudding', 'Rice Krispy Treats'
+
 
     }, "Cafe Ventana": {
 
