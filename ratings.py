1625ffedd2324b4f9ce455a0efc1e34e8af33f8b
Mo Al Elew <malelew@ucsd.edu>
2015-08-28 10:00:26 -0700

    fixed ratings.py

--- ratings.py
+++ ratings.py
@@ -1,17 +1,16 @@
 ratings = {"Canyon Vista": {
-    'Assorted Summer Scones' = True, 'Beans' = True, 'Blueberry Muffin' = True, 'Cage Free Scrambled Eggs' = True, 'Carrots & Jalapenos' = True, 'Flatbread Breakfast Pizza' = True,
-    'Steelcut Oatmeal' = True, 'Tater Barrels' = True, 'Veggie Sausage' = True,
-    'Whole Wheat English Muffin' = True, 'Zucchini Breakfast Muffins' = True,
-    'Fresh Tortillas' = True, 'Gluten Friendly Bread' = True, 'Pancakes' = True, 'Pork Sausage Links' = True,
-    'Scrambled Egg Whites' = True, 'Smoked Bacon' = True, 'Cereal with Milk' = True, 'Cut Fresh Fruit' = True, 'Whole Apple' = True, 'Whole Banana' = True,
-    'Whole Orange' = True, 'Yogurt Bar' = True, 'Chciken Picatta' = False, 'Cheese Enchilada' = False, 'Aztec Black Beans' = False, 'Cilantro Rice' = False, 'Steamed Broccoli w/ Almonds' = False,
-    'Summer Roast Beef Wrap' = False, 'Tuna Super Sub' = False, 'Vegetarian Super Sub' = False, 'Cheeseburger' = False, 'Gardein Burger' = False, 'Grilled Cheese Sandwich' = False, 'Hamburger' = False,
-    'Hot Dog' = False, 'Crispy French Fries' = False, 'Cheese Pizza' = False, 'Pepperoni Pizza' = False, 'Vegetarian Pizza' = False, 'BYO Pasta Bar (Alfredo Nutritional Sample)' = False, 'Fresh Salad Bar' = False,
-    'Jalapeno Cornbread' = False, 'Clam Chowder' = False, 'Vegetarian Chili' = False, 'Butterscotch Pudding' = False, 'Chocolate Cake' = False, 'Chocolate Pudding' = False, 'Rice Krispy Treats' = False,
-    'Herb Crusted Red Snapper' = False, 'Homestyle Pot Roast' = False, 'Baked Potato Bar' = False, 'Steamed Brown Rice' = False, 'Sweet Chili Carrots' = False, 'Cheese Pizza' = False, 'Pepperoni Pizza' = False,
-    'Vegetarian Pizza' = False, 'BYO Pasta Bar (Alfredo Nutritional Sample)' = False, 'Fresh Salad Bar' = False, 'Jalapeno Cornbread' = False, 'Clam Chowder' = False, 'Vegetarian Chili' = False,
-    'Butterscotch Pudding' = False, 'Chocolate Cake' = False, 'Chocolate Pudding' = False, 'Rice Krispy Treats' = False
-
+    'Assorted Summer Scones' : True, 'Beans' : True, 'Blueberry Muffin' : True, 'Cage Free Scrambled Eggs' : True, 'Carrots & Jalapenos' : True, 'Flatbread Breakfast Pizza' : True,
+    'Steelcut Oatmeal' : True, 'Tater Barrels' : True, 'Veggie Sausage' : True,
+    'Whole Wheat English Muffin' : True, 'Zucchini Breakfast Muffins' : True,
+    'Fresh Tortillas' : True, 'Gluten Friendly Bread' : True, 'Pancakes' : True, 'Pork Sausage Links' : True,
+    'Scrambled Egg Whites' : True, 'Smoked Bacon' : True, 'Cereal with Milk' : True, 'Cut Fresh Fruit' : True, 'Whole Apple' : True, 'Whole Banana' : True,
+    'Whole Orange' : True, 'Yogurt Bar' : True, 'Chciken Picatta' : False, 'Cheese Enchilada' : False, 'Aztec Black Beans' : False, 'Cilantro Rice' : False, 'Steamed Broccoli w/ Almonds' : False,
+    'Summer Roast Beef Wrap' : False, 'Tuna Super Sub' : False, 'Vegetarian Super Sub' : False, 'Cheeseburger' : False, 'Gardein Burger' : False, 'Grilled Cheese Sandwich' : False, 'Hamburger' : False,
+    'Hot Dog' : False, 'Crispy French Fries' : False, 'Cheese Pizza' : False, 'Pepperoni Pizza' : False, 'Vegetarian Pizza' : False, 'BYO Pasta Bar (Alfredo Nutritional Sample)' : False, 'Fresh Salad Bar' : False,
+    'Jalapeno Cornbread' : False, 'Clam Chowder' : False, 'Vegetarian Chili' : False, 'Butterscotch Pudding' : False, 'Chocolate Cake' : False, 'Chocolate Pudding' : False, 'Rice Krispy Treats' : False,
+    'Herb Crusted Red Snapper' : False, 'Homestyle Pot Roast' : False, 'Baked Potato Bar' : False, 'Steamed Brown Rice' : False, 'Sweet Chili Carrots' : False, 'Cheese Pizza' : False, 'Pepperoni Pizza' : False,
+    'Vegetarian Pizza' : False, 'BYO Pasta Bar (Alfredo Nutritional Sample)' : False, 'Fresh Salad Bar' : False, 'Jalapeno Cornbread' : False, 'Clam Chowder' : False, 'Vegetarian Chili' : False,
+    'Butterscotch Pudding' : False, 'Chocolate Cake' : False, 'Chocolate Pudding' : False, 'Rice Krispy Treats' : False
 
     }, "Cafe Ventana": {
 
